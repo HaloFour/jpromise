5547a9f548f319007b12742e28b421435d959d37
Spindler, Justin <Justin.Spindler@inin.com>
2014-12-07 13:43:59 -0500

    Move default executor to a constant (allows easily replacing it later), change resolved/rejected to return the actual DeferredPromise instance (saves the no-op method call)

--- core/src/main/java/org/jpromise/Promise.java
+++ core/src/main/java/org/jpromise/Promise.java
@@ -8,6 +8,8 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
 public abstract class Promise<V> implements Future<V> {
+    public static final Executor DEFAULT_EXECUTOR = CurrentThreadExecutor.INSTANCE;
+
     public abstract PromiseState state();
 
     public boolean isDone() {
@@ -35,23 +37,23 @@ public abstract class Promise<V> implements Future<V> {
     }
 
     public Promise<V> then(OnResolved<? super V> action) {
-        return this.then(CurrentThreadExecutor.INSTANCE, action);
+        return this.then(DEFAULT_EXECUTOR, action);
     }
 
     public <V_APPLIED> Promise<V_APPLIED> thenApply(OnResolvedFunction<? super V, ? extends V_APPLIED> function) {
-        return this.thenApply(CurrentThreadExecutor.INSTANCE, function);
+        return this.thenApply(DEFAULT_EXECUTOR, function);
     }
 
     public <V_COMPOSED> Promise<V_COMPOSED> thenCompose(OnResolvedFunction<? super V, ? extends Future<V_COMPOSED>> function) {
-        return this.thenCompose(CurrentThreadExecutor.INSTANCE, function);
+        return this.thenCompose(DEFAULT_EXECUTOR, function);
     }
 
     public Promise<V> rejected(OnRejected<Throwable> action) {
-        return this.rejected(Throwable.class, CurrentThreadExecutor.INSTANCE, action);
+        return this.rejected(Throwable.class, DEFAULT_EXECUTOR, action);
     }
 
     public <E extends Throwable> Promise<V> rejected(Class<E> exceptionClass, OnRejected<? super E> action) {
-        return this.rejected(exceptionClass, CurrentThreadExecutor.INSTANCE, action);
+        return this.rejected(exceptionClass, DEFAULT_EXECUTOR, action);
     }
 
     public Promise<V> rejected(Executor executor, OnRejected<Throwable> action) {
@@ -59,7 +61,7 @@ public abstract class Promise<V> implements Future<V> {
     }
 
     public Promise<V> handleWith(OnRejectedHandler<Throwable, ? extends V> handler) {
-        return this.handleWith(Throwable.class, CurrentThreadExecutor.INSTANCE, handler);
+        return this.handleWith(Throwable.class, DEFAULT_EXECUTOR, handler);
     }
 
     public Promise<V> handleWith(Executor executor, OnRejectedHandler<Throwable, ? extends V> handler) {
@@ -67,11 +69,11 @@ public abstract class Promise<V> implements Future<V> {
     }
 
     public <E extends Throwable> Promise<V> handleWith(Class<E> exceptionClass, OnRejectedHandler<? super E, ? extends V> handler) {
-        return this.handleWith(exceptionClass, CurrentThreadExecutor.INSTANCE, handler);
+        return this.handleWith(exceptionClass, DEFAULT_EXECUTOR, handler);
     }
 
     public Promise<V> fallbackWith(OnRejectedHandler<Throwable, ? extends Future<V>> fallback) {
-        return this.fallbackWith(Throwable.class, CurrentThreadExecutor.INSTANCE, fallback);
+        return this.fallbackWith(Throwable.class, DEFAULT_EXECUTOR, fallback);
     }
 
     public Promise<V> fallbackWith(Executor executor, OnRejectedHandler<Throwable, ? extends Future<V>> fallback) {
@@ -79,11 +81,11 @@ public abstract class Promise<V> implements Future<V> {
     }
 
     public <E extends Throwable> Promise<V> fallbackWith(Class<E> exceptionClass, OnRejectedHandler<? super E, ? extends Future<V>> fallback) {
-        return this.fallbackWith(exceptionClass, CurrentThreadExecutor.INSTANCE, fallback);
+        return this.fallbackWith(exceptionClass, DEFAULT_EXECUTOR, fallback);
     }
 
     public Promise<V> whenCompleted(OnCompleted<V> action) {
-        return this.whenCompleted(CurrentThreadExecutor.INSTANCE, action);
+        return this.whenCompleted(DEFAULT_EXECUTOR, action);
     }
 
     public abstract Promise<V> then(Executor executor, OnResolved<? super V> action);
@@ -95,14 +97,14 @@ public abstract class Promise<V> implements Future<V> {
     public abstract Promise<V> whenCompleted(Executor executor, OnCompleted<V> action);
 
     public static <V> Promise<V> resolved(V result) {
-        Deferred<V> deferred = new DeferredPromise<>();
-        deferred.resolve(result);
-        return deferred.promise();
+        DeferredPromise<V> promise = new DeferredPromise<>();
+        promise.resolve(result);
+        return promise;
     }
 
     public static <V> Promise<V> rejected(Throwable exception) {
-        Deferred<V> deferred = new DeferredPromise<>();
-        deferred.reject(exception);
-        return deferred.promise();
+        DeferredPromise<V> promise = new DeferredPromise<>();
+        promise.reject(exception);
+        return promise;
     }
 }
