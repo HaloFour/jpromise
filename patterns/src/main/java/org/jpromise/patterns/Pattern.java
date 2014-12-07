f2f5cd36bb7868c634d388b645e6cabf646492a7
Spindler, Justin <Justin.Spindler@inin.com>
2014-12-07 16:31:52 -0500

    Removed unnecessary code, changed name of "then" method to "spread" and added pattern element count to reduce overload confusion

--- patterns/src/main/java/org/jpromise/patterns/Pattern.java
+++ patterns/src/main/java/org/jpromise/patterns/Pattern.java
@@ -21,59 +21,41 @@ public class Pattern {
     private Pattern() { }
 
     public static <V1, V2> Promise<Pattern2<V1, V2>> join(final Promise<V1> first, final Promise<V2> second) {
-        List<Promise<?>> promises = new ArrayList<Promise<?>>(2);
-        promises.add(first);
-        promises.add(second);
-        return PromiseManager.whenAllResolved(promises)
+        return PromiseManager.whenAllResolved(first, second)
                 .thenApply(new OnResolvedFunction<Void, Pattern2<V1, V2>>() {
                     @Override
                     public Pattern2<V1, V2> resolved(Void result) throws Throwable {
-                        return new Pattern2<V1, V2>(getResult(first), getResult(second));
+                        return new Pattern2<>(getResult(first), getResult(second));
                     }
                 });
     }
 
     public static <V1, V2, V3> Promise<Pattern3<V1, V2, V3>> join(final Promise<V1> first, final Promise<V2> second, final Promise<V3> third) {
-        List<Promise<?>> promises = new ArrayList<Promise<?>>(3);
-        promises.add(first);
-        promises.add(second);
-        promises.add(third);
-        return PromiseManager.whenAllResolved(promises)
+        return PromiseManager.whenAllResolved(first, second, third)
                 .thenApply(new OnResolvedFunction<Void, Pattern3<V1, V2, V3>>() {
                     @Override
                     public Pattern3<V1, V2, V3> resolved(Void result) throws Throwable {
-                        return new Pattern3<V1, V2, V3>(getResult(first), getResult(second), getResult(third));
+                        return new Pattern3<>(getResult(first), getResult(second), getResult(third));
                     }
                 });
     }
 
     public static <V1, V2, V3, V4> Promise<Pattern4<V1, V2, V3, V4>> join(final Promise<V1> first, final Promise<V2> second, final Promise<V3> third, final Promise<V4> fourth) {
-        List<Promise<?>> promises = new ArrayList<Promise<?>>(4);
-        promises.add(first);
-        promises.add(second);
-        promises.add(third);
-        promises.add(fourth);
-        return PromiseManager.whenAllResolved(promises)
+        return PromiseManager.whenAllResolved(first, second, third, fourth)
                 .thenApply(new OnResolvedFunction<Void, Pattern4<V1, V2, V3, V4>>() {
                     @Override
                     public Pattern4<V1, V2, V3, V4> resolved(Void result) throws Throwable {
-                        return new Pattern4<V1, V2, V3, V4>(getResult(first), getResult(second), getResult(third), getResult(fourth));
+                        return new Pattern4<>(getResult(first), getResult(second), getResult(third), getResult(fourth));
                     }
                 });
     }
 
     public static <V1, V2, V3, V4, V5> Promise<Pattern5<V1, V2, V3, V4, V5>> join(final Promise<V1> first, final Promise<V2> second, final Promise<V3> third, final Promise<V4> fourth, final Promise<V5> fifth) {
-        List<Promise<?>> promises = new ArrayList<Promise<?>>(4);
-        promises.add(first);
-        promises.add(second);
-        promises.add(third);
-        promises.add(fourth);
-        promises.add(fifth);
-        return PromiseManager.whenAllResolved(promises)
+        return PromiseManager.whenAllResolved(first, second, third, fourth, fifth)
                 .thenApply(new OnResolvedFunction<Void, Pattern5<V1, V2, V3, V4, V5>>() {
                     @Override
                     public Pattern5<V1, V2, V3, V4, V5> resolved(Void result) throws Throwable {
-                        return new Pattern5<V1, V2, V3, V4, V5>(getResult(first), getResult(second), getResult(third), getResult(fourth), getResult(fifth));
+                        return new Pattern5<>(getResult(first), getResult(second), getResult(third), getResult(fourth), getResult(fifth));
                     }
                 });
     }
@@ -85,51 +67,51 @@ public class Pattern {
         return promise.get();
     }
 
-    public static <V1, V2> OnResolved<Pattern2<V1, V2>> then(OnResolved2<V1, V2> action) {
+    public static <V1, V2> OnResolved<Pattern2<V1, V2>> spread2(OnResolved2<V1, V2> action) {
         return action;
     }
 
-    public static <V1, V2, V3> OnResolved<Pattern3<V1, V2, V3>> then(OnResolved3<V1, V2, V3> action) {
+    public static <V1, V2, V3> OnResolved<Pattern3<V1, V2, V3>> spread3(OnResolved3<V1, V2, V3> action) {
         return action;
     }
 
-    public static <V1, V2, V3, V4> OnResolved<Pattern4<V1, V2, V3, V4>> then(OnResolved4<V1, V2, V3, V4> action) {
+    public static <V1, V2, V3, V4> OnResolved<Pattern4<V1, V2, V3, V4>> spread4(OnResolved4<V1, V2, V3, V4> action) {
         return action;
     }
 
-    public static <V1, V2, V3, V4, V5> OnResolved<Pattern5<V1, V2, V3, V4, V5>> then(OnResolved5<V1, V2, V3, V4, V5> action) {
+    public static <V1, V2, V3, V4, V5> OnResolved<Pattern5<V1, V2, V3, V4, V5>> spread5(OnResolved5<V1, V2, V3, V4, V5> action) {
         return action;
     }
 
-    public static <V1, V2, VR> OnResolvedFunction<Pattern2<V1, V2>, VR> apply(OnResolvedFunction2<V1, V2, VR> function) {
+    public static <V1, V2, VR> OnResolvedFunction<Pattern2<V1, V2>, VR> apply2(OnResolvedFunction2<V1, V2, VR> function) {
         return function;
     }
 
-    public static <V1, V2, V3, VR> OnResolvedFunction<Pattern3<V1, V2, V3>, VR> apply(OnResolvedFunction3<V1, V2, V3, VR> function) {
+    public static <V1, V2, V3, VR> OnResolvedFunction<Pattern3<V1, V2, V3>, VR> apply3(OnResolvedFunction3<V1, V2, V3, VR> function) {
         return function;
     }
 
-    public static <V1, V2, V3, V4, VR> OnResolvedFunction<Pattern4<V1, V2, V3, V4>, VR> apply(OnResolvedFunction4<V1, V2, V3, V4, VR> function) {
+    public static <V1, V2, V3, V4, VR> OnResolvedFunction<Pattern4<V1, V2, V3, V4>, VR> apply4(OnResolvedFunction4<V1, V2, V3, V4, VR> function) {
         return function;
     }
 
-    public static <V1, V2, V3, V4, V5, VR> OnResolvedFunction<Pattern5<V1, V2, V3, V4, V5>, VR> apply(OnResolvedFunction5<V1, V2, V3, V4, V5, VR> function) {
+    public static <V1, V2, V3, V4, V5, VR> OnResolvedFunction<Pattern5<V1, V2, V3, V4, V5>, VR> apply5(OnResolvedFunction5<V1, V2, V3, V4, V5, VR> function) {
         return function;
     }
 
-    public static <V1, V2, VR> OnResolvedFunction<Pattern2<V1, V2>, ? extends Future<VR>> compose(OnResolvedFunction2<V1, V2, ? extends Future<VR>> function) {
+    public static <V1, V2, VR> OnResolvedFunction<Pattern2<V1, V2>, ? extends Future<VR>> compose2(OnResolvedFunction2<V1, V2, ? extends Future<VR>> function) {
         return function;
     }
 
-    public static <V1, V2, V3, VR> OnResolvedFunction<Pattern3<V1, V2, V3>, ? extends Future<VR>> compose(OnResolvedFunction3<V1, V2, V3, ? extends Future<VR>> function) {
+    public static <V1, V2, V3, VR> OnResolvedFunction<Pattern3<V1, V2, V3>, ? extends Future<VR>> compose3(OnResolvedFunction3<V1, V2, V3, ? extends Future<VR>> function) {
         return function;
     }
 
-    public static <V1, V2, V3, V4, VR> OnResolvedFunction<Pattern4<V1, V2, V3, V4>, ? extends Future<VR>> compose(OnResolvedFunction4<V1, V2, V3, V4, ? extends Future<VR>> function) {
+    public static <V1, V2, V3, V4, VR> OnResolvedFunction<Pattern4<V1, V2, V3, V4>, ? extends Future<VR>> compose4(OnResolvedFunction4<V1, V2, V3, V4, ? extends Future<VR>> function) {
         return function;
     }
 
-    public static <V1, V2, V3, V4, V5, VR> OnResolvedFunction<Pattern5<V1, V2, V3, V4, V5>, ? extends Future<VR>> compose(OnResolvedFunction5<V1, V2, V3, V4, V5, ? extends Future<VR>> function) {
+    public static <V1, V2, V3, V4, V5, VR> OnResolvedFunction<Pattern5<V1, V2, V3, V4, V5>, ? extends Future<VR>> compose5(OnResolvedFunction5<V1, V2, V3, V4, V5, ? extends Future<VR>> function) {
         return function;
     }
 }
--- patterns/src/test/java/org/jpromise/patterns/PatternTest.java
+++ patterns/src/test/java/org/jpromise/patterns/PatternTest.java
@@ -1,8 +1,10 @@
 package org.jpromise.patterns;
 
 import org.jpromise.Promise;
-import org.jpromise.functions.OnResolved2;
+import org.jpromise.functions.*;
 import org.junit.Test;
+
+import java.util.concurrent.Future;
 
 import static org.jpromise.PromiseHelpers.*;
 import static org.junit.Assert.assertEquals;
@@ -14,65 +16,213 @@ public class PatternTest {
     private static final String SUCCESS4 = "SUCCESS4";
     private static final String SUCCESS5 = "SUCCESS5";
 
+    private Promise<Pattern2<String, String>> get2() {
+        return Pattern.join(resolveAfter(SUCCESS1, 10), resolveAfter(SUCCESS2, 10));
+    }
+
+    private Promise<Pattern3<String, String, String>> get3() {
+        return Pattern.join(resolveAfter(SUCCESS1, 10), resolveAfter(SUCCESS2, 10), resolveAfter(SUCCESS3, 10));
+    }
+
+    private Promise<Pattern4<String, String, String, String>> get4() {
+        return Pattern.join(resolveAfter(SUCCESS1, 10), resolveAfter(SUCCESS2, 10), resolveAfter(SUCCESS3, 10), resolveAfter(SUCCESS4, 10));
+    }
+
+    private Promise<Pattern5<String, String, String, String, String>> get5() {
+        return Pattern.join(resolveAfter(SUCCESS1, 10), resolveAfter(SUCCESS2, 10), resolveAfter(SUCCESS3, 10), resolveAfter(SUCCESS4, 10), resolveAfter(SUCCESS5, 10));
+    }
+
     @Test
     public void join2() throws Throwable {
-        Promise<String> promise1 = resolveAfter(SUCCESS1, 10);
-        Promise<String> promise2 = resolveAfter(SUCCESS2, 10);
-
-        Promise<Pattern2<String, String>> pattern = Pattern.join(promise1, promise2);
-
-        assertResolves(new Pattern2<>(SUCCESS1, SUCCESS2), pattern);
+        assertResolves(new Pattern2<>(SUCCESS1, SUCCESS2), get2());
     }
 
     @Test
     public void join3() throws Throwable {
-        Promise<String> promise1 = resolveAfter(SUCCESS1, 10);
-        Promise<String> promise2 = resolveAfter(SUCCESS2, 10);
-        Promise<String> promise3 = resolveAfter(SUCCESS3, 10);
-
-        Promise<Pattern3<String, String, String>> pattern = Pattern.join(promise1, promise2, promise3);
-
-        assertResolves(new Pattern3<>(SUCCESS1, SUCCESS2, SUCCESS3), pattern);
+        assertResolves(new Pattern3<>(SUCCESS1, SUCCESS2, SUCCESS3), get3());
     }
 
     @Test
     public void join4() throws Throwable {
-        Promise<String> promise1 = resolveAfter(SUCCESS1, 10);
-        Promise<String> promise2 = resolveAfter(SUCCESS2, 10);
-        Promise<String> promise3 = resolveAfter(SUCCESS3, 10);
-        Promise<String> promise4 = resolveAfter(SUCCESS4, 10);
-
-        Promise<Pattern4<String, String, String, String>> pattern = Pattern.join(promise1, promise2, promise3, promise4);
-
-        assertResolves(new Pattern4<>(SUCCESS1, SUCCESS2, SUCCESS3, SUCCESS4), pattern);
+        assertResolves(new Pattern4<>(SUCCESS1, SUCCESS2, SUCCESS3, SUCCESS4), get4());
     }
 
     @Test
     public void join5() throws Throwable {
-        Promise<String> promise1 = resolveAfter(SUCCESS1, 10);
-        Promise<String> promise2 = resolveAfter(SUCCESS2, 10);
-        Promise<String> promise3 = resolveAfter(SUCCESS3, 10);
-        Promise<String> promise4 = resolveAfter(SUCCESS4, 10);
-        Promise<String> promise5 = resolveAfter(SUCCESS5, 10);
-
-        Promise<Pattern5<String, String, String, String, String>> pattern = Pattern.join(promise1, promise2, promise3, promise4, promise5);
-
-        assertResolves(new Pattern5<>(SUCCESS1, SUCCESS2, SUCCESS3, SUCCESS4, SUCCESS5), pattern);
+        assertResolves(new Pattern5<>(SUCCESS1, SUCCESS2, SUCCESS3, SUCCESS4, SUCCESS5), get5());
     }
 
     @Test
-    public void resolved2() throws Throwable {
-        Promise<String> promise1 = resolveAfter(SUCCESS1, 10);
-        Promise<String> promise2 = resolveAfter(SUCCESS2, 10);
-
-        Promise<Pattern2<String, String>> pattern = Pattern.join(promise1, promise2);
-
-        pattern.then(Pattern.then(new OnResolved2<String, String>() {
-            @Override
+    public void spread2() throws Throwable {
+        get2().then(Pattern.spread2(new OnResolved2<String, String>() {
+                @Override
             public void resolved(String item1, String item2) throws Throwable {
                 assertEquals(SUCCESS1, item1);
                 assertEquals(SUCCESS2, item2);
             }
         })).get();
+    }
+
+    @Test
+    public void spread3() throws Throwable {
+        get3().then(Pattern.spread3(new OnResolved3<String, String, String>() {
+            @Override
+            public void resolved(String item1, String item2, String item3) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+            }
+        })).get();
+    }
+
+    @Test
+    public void spread4() throws Throwable {
+        get4().then(Pattern.spread4(new OnResolved4<String, String, String, String>() {
+            @Override
+            public void resolved(String item1, String item2, String item3, String item4) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                assertEquals(SUCCESS4, item4);
+            }
+        })).get();
+    }
+
+    @Test
+    public void spread5() throws Throwable {
+        get5().then(Pattern.spread5(new OnResolved5<String, String, String, String, String>() {
+            @Override
+            public void resolved(String item1, String item2, String item3, String item4, String item5) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                assertEquals(SUCCESS4, item4);
+                assertEquals(SUCCESS5, item5);
+            }
+        })).get();
+    }
+
+    @Test
+    public void apply2() throws Throwable {
+        Promise<String> promise = get2().thenApply(Pattern.apply2(new OnResolvedFunction2<String, String, String>() {
+            @Override
+            public String resolved(String item1, String item2) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                return item1 + item2;
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2, promise);
+    }
+
+    @Test
+    public void apply3() throws Throwable {
+        Promise<String> promise = get3().thenApply(Pattern.apply3(new OnResolvedFunction3<String, String, String, String>() {
+            @Override
+            public String resolved(String item1, String item2, String item3) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                return item1 + item2 + item3;
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2 + SUCCESS3, promise);
+    }
+
+    @Test
+    public void apply4() throws Throwable {
+        Promise<String> promise = get4().thenApply(Pattern.apply4(new OnResolvedFunction4<String, String, String, String, String>() {
+            @Override
+            public String resolved(String item1, String item2, String item3, String item4) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                assertEquals(SUCCESS4, item4);
+                return item1 + item2 + item3 + item4;
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2 + SUCCESS3 + SUCCESS4, promise);
+    }
+
+    @Test
+    public void apply5() throws Throwable {
+        Promise<String> promise = get5().thenApply(Pattern.apply5(new OnResolvedFunction5<String, String, String, String, String, String>() {
+            @Override
+            public String resolved(String item1, String item2, String item3, String item4, String item5) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                assertEquals(SUCCESS4, item4);
+                assertEquals(SUCCESS5, item5);
+                return item1 + item2 + item3 + item4 + item5;
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2 + SUCCESS3 + SUCCESS4 + SUCCESS5, promise);
+    }
+
+    @Test
+    public void compose2() throws Throwable {
+        Promise<String> promise = get2().thenCompose(Pattern.compose2(new OnResolvedFunction2<String, String, Future<String>>() {
+            @Override
+            public Future<String> resolved(String item1, String item2) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                return resolveAfter(item1 + item2, 10);
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2, promise);
+    }
+
+    @Test
+    public void compose3() throws Throwable {
+        Promise<String> promise = get3().thenCompose(Pattern.apply3(new OnResolvedFunction3<String, String, String, Future<String>>() {
+            @Override
+            public Future<String> resolved(String item1, String item2, String item3) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                return resolveAfter(item1 + item2 + item3, 10);
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2 + SUCCESS3, promise);
+    }
+
+    @Test
+    public void compose4() throws Throwable {
+        Promise<String> promise = get4().thenCompose(Pattern.compose4(new OnResolvedFunction4<String, String, String, String, Future<String>>() {
+            @Override
+            public Future<String> resolved(String item1, String item2, String item3, String item4) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                assertEquals(SUCCESS4, item4);
+                return resolveAfter(item1 + item2 + item3 + item4, 10);
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2 + SUCCESS3 + SUCCESS4, promise);
+    }
+
+    @Test
+    public void compose5() throws Throwable {
+        Promise<String> promise = get5().thenCompose(Pattern.compose5(new OnResolvedFunction5<String, String, String, String, String, Future<String>>() {
+            @Override
+            public Future<String> resolved(String item1, String item2, String item3, String item4, String item5) throws Throwable {
+                assertEquals(SUCCESS1, item1);
+                assertEquals(SUCCESS2, item2);
+                assertEquals(SUCCESS3, item3);
+                assertEquals(SUCCESS4, item4);
+                assertEquals(SUCCESS5, item5);
+                return resolveAfter(item1 + item2 + item3 + item4 + item5, 10);
+            }
+        }));
+
+        assertResolves(SUCCESS1 + SUCCESS2 + SUCCESS3 + SUCCESS4 + SUCCESS5, promise);
     }
 }
