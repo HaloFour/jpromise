fc1e295da10c3e99d8d910f149a503c4bc1dfa65
Spindler, Justin <Justin.Spindler@inin.com>
2014-12-09 19:48:08 -0500

    Add jpromise-completable module to support converting to/from Promise<V> and CompletionStage<V>

---
+++ completable/src/main/java/org/jpromise/CompletionPromise.java
@@ -0,0 +1,36 @@
+package org.jpromise;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static org.jpromise.util.MessageUtil.mustNotBeNull;
+
+public class CompletionPromise<V> extends AbstractPromise<V> {
+    public CompletionPromise(final CompletionStage<V> completionStage) {
+        if (completionStage == null) throw new IllegalArgumentException(mustNotBeNull("completionStage"));
+        completionStage.whenCompleteAsync((result, exception) -> {
+            if (exception != null) {
+                completeWithException(exception);
+            }
+            else {
+                complete(result);
+            }
+        });
+    }
+
+    public static <V> CompletionStage<V> toCompletionStage(Promise<V> promise) {
+        if (promise == null) throw new IllegalArgumentException(mustNotBeNull("promise"));
+        CompletableFuture<V> future = new CompletableFuture<>();
+        promise.whenCompleted((p, result, exception) -> {
+            switch (p.state()) {
+                case RESOLVED:
+                    future.complete(result);
+                    break;
+                case REJECTED:
+                    future.completeExceptionally(exception);
+                    break;
+            }
+        });
+        return future;
+    }
+}
---
+++ completable/src/test/java/org/jpromise/CompletionPromiseTest.java
@@ -0,0 +1,63 @@
+package org.jpromise;
+
+import junit.framework.AssertionFailedError;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import static org.jpromise.PromiseHelpers.*;
+import static org.junit.Assert.*;
+
+public class CompletionPromiseTest {
+    private static final String SUCCESS1 = "SUCCESS1";
+
+    @Test
+    public void completionPromiseResolves() throws Throwable {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        CompletionPromise<String> promise = new CompletionPromise<>(future);
+
+        future.complete(SUCCESS1);
+
+        assertResolves(SUCCESS1, promise);
+    }
+
+    @Test
+    public void completionPromiseRejects() throws Throwable {
+        Throwable exception = new Throwable();
+        CompletableFuture<String> future = new CompletableFuture<>();
+        CompletionPromise<String> promise = new CompletionPromise<>(future);
+
+        future.completeExceptionally(exception);
+
+        assertRejects(exception, promise);
+    }
+
+    @Test
+    public void completionStageCompletes() throws Throwable {
+        Promise<String> promise = resolveAfter(SUCCESS1, 10);
+        CompletionStage<String> completionStage = CompletionPromise.toCompletionStage(promise);
+        CompletableFuture<String> future = completionStage.toCompletableFuture();
+
+        String result = future.get();
+
+        assertEquals(SUCCESS1, result);
+    }
+
+    @Test
+    public void completionStageCompletesExceptionally() throws Throwable {
+        Throwable exception = new Throwable();
+        Promise<String> promise = rejectAfter(exception, 10);
+        CompletionStage<String> completionStage = CompletionPromise.toCompletionStage(promise);
+        CompletableFuture<String> future = completionStage.toCompletableFuture();
+
+        try {
+            String ignored = future.get();
+            throw new AssertionFailedError("future.get() should not complete successfully");
+        }
+        catch (ExecutionException executionException) {
+            assertEquals(exception, executionException.getCause());
+        }
+    }
+}
