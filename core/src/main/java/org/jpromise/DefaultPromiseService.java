3d9d4ae19b368f14bf3c90d891b171ea06ac5212
Spindler, Justin <Justin.Spindler@inin.com>
2014-12-27 15:39:43 -0500

    Have DefaultPromiseService extend AbstractPromiseService rather than ExecutorPromiseService

--- core/src/main/java/org/jpromise/DefaultPromiseService.java
+++ core/src/main/java/org/jpromise/DefaultPromiseService.java
@@ -4,11 +4,14 @@ package org.jpromise;
  * A {@link org.jpromise.PromiseService} that uses the {@link org.jpromise.PromiseExecutors#DEFAULT_CREATION_EXECUTOR}
  * promise executor to execute tasks.
  */
-public class DefaultPromiseService extends ExecutorPromiseService {
+public class DefaultPromiseService extends AbstractPromiseService {
     /**
-     * Creates a new {@link org.jpromise.DefaultPromiseService}.
+     * Executes the {@link java.lang.Runnable} task on the {@link org.jpromise.PromiseExecutors#DEFAULT_CREATION_EXECUTOR}
+     * executor.
+     * @param task The command to be executed.
      */
-    public DefaultPromiseService() {
-        super(PromiseExecutors.DEFAULT_CREATION_EXECUTOR);
+    @Override
+    protected void execute(Runnable task) {
+        PromiseExecutors.DEFAULT_CREATION_EXECUTOR.execute(task);
     }
 }
